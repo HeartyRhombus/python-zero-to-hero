a7f3acf384ca0fe600e4514b80fb6a2d4b1f9030
HeartyRhombus <kaylahmg@gmail.com>
2021-06-17 17:31:19 -0700

    add solution to exercise

---
+++ objectAndDataStructureBasics/codingExercise1-numbers-simpleArithmetic.py
@@ -0,0 +1,5 @@
+# Write an expression that equals 100.
+# For example 50+50 or 110-10.
+# See if you can use more than one arithmetic operator! Write only one expression and submit only one line of code.
+
+(((2**2)+1)**3)-25
