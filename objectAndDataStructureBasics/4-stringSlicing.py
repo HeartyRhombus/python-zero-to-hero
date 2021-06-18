724471099058ee9878e9fc826fa396408622110e
HeartyRhombus <kaylahmg@gmail.com>
2021-06-18 09:56:29 -0700

    add solution to exercise 4

---
+++ objectAndDataStructureBasics/4-stringSlicing.py
@@ -0,0 +1,6 @@
+# Use string slicing to grab the word 'ink' from inside 'tinker'.
+# Remember that when slicing you only go up to but not including the index.
+# You should only write one line of code for this. Do not assign a variable name to the string.
+'tinker'[1:4]
+# Can also use 'tinker'[1:4:1]
+# the step value of 1 however is implied and is only needed if greater than one or a negative value.
