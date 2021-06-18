54d14bceb99ec28d42511f19ac67fd29379afecb
HeartyRhombus <kaylahmg@gmail.com>
2021-06-18 09:52:40 -0700

    add solution to exercise 3

---
+++ objectAndDataStructureBasics/3-stringIndexing.py
@@ -0,0 +1,3 @@
+# Write a string index that returns just the letter 'r' from 'Hello World'.
+# You should only write one line of code for this. Do not assign a variable name to the string.
+'Hello World'[8]
