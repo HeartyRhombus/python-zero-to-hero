5c263d1761720883a2eb79f950b16322fe68b47d
HeartyRhombus <kaylahmg@gmail.com>
2021-06-18 10:43:58 -0700

    add solution to exercise 5

---
+++ objectAndDataStructureBasics/5-printFormatting.py
@@ -0,0 +1,6 @@
+# Write an expression using any of the string formatting methods we have learned (except f-strings)
+# to return the phrase 'Python rules!'
+# Your solution should be entered on one line.
+# You can not use variable names, only the strings.
+
+print('{} {}!'.format('Python', 'rules'))
