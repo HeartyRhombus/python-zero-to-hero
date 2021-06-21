1528a504e7a57601148cd320c370b85f36523421
HeartyRhombus <kaylahmg@gmail.com>
2021-06-21 15:57:10 -0700

    add solution to exercise 7

---
+++ objectAndDataStructureBasics/7-dictionaries.py
@@ -0,0 +1,3 @@
+# Create a dictionary where all the keys are strings, and all the values are integers.
+# Just write the dictionary on a single line, don't assign a variable name to the dictionary.
+{"members": 17, "announcements": 4}
