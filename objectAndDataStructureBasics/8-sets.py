068a8d996ec318b12ead4c5a04274ee76725a6de
HeartyRhombus <kaylahmg@gmail.com>
2021-06-21 16:14:34 -0700

    add solution for exercise 8

---
+++ objectAndDataStructureBasics/8-sets.py
@@ -0,0 +1,4 @@
+# Create an expression that would turn the string 'Mississippi' into a set of unique letters.
+# You should only write one line of code for this.
+# Do not assign a variable name to the set.
+set('Mississippi')
