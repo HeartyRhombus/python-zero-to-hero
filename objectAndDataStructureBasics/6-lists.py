51e77d151f9aac52e8dbcb5ffbaf057c64bf7937
HeartyRhombus <kaylahmg@gmail.com>
2021-06-21 15:17:07 -0700

    add solution to exercise 6

---
+++ objectAndDataStructureBasics/6-lists.py
@@ -0,0 +1,5 @@
+# Create a list that contains at least one string, one integer and one float.
+# Note that the order and number of items doesn't matter.
+# The answer should just be one list on a single line.
+# Don't assign a variable name to the list.
+["I'm a string", 42, 3.1415]
