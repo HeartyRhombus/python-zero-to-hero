3d23c00ac0b1b2b9fd7ab9a9ec1d2fa8f40f78d0
HeartyRhombus <kaylahmg@gmail.com>
2021-07-03 13:38:29 -0700

    add solution to exercise 12

--- methodsAndFunctions/12-simpleBoolean.py
+++ methodsAndFunctions/12-simpleBoolean.py
@@ -1,2 +1,8 @@
 # Define a function called myfunc that takes in a Boolean value (True or False). If True, return 'Hello', and if False, return 'Goodbye'.
 # Remember, don't run the function, simply provide the definition.
+
+def myfunc(a):
+    if a == True:
+        return 'Hello'
+    elif a == False:
+        return 'Goodbye'
