e3e86b3fa8d8e89484d6026afbe59c2f5c725502
HeartyRhombus <kaylahmg@gmail.com>
2021-07-03 13:40:52 -0700

    add solution to exercise 15

--- methodsAndFunctions/15-isEven.py
+++ methodsAndFunctions/15-isEven.py
@@ -1,2 +1,8 @@
 # Define a function called is_even that takes in one argument, and returns True if the passed-in value is even, False if it is not.
 # Remember, don't run the funciton, simply provide the definition.
+
+def is_even(arg):
+    if arg % 2 == 0:
+        return True
+    else:
+        return False
