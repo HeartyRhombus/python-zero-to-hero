2d78adafa53a9ba39400627bda293116ce2f4576
HeartyRhombus <kaylahmg@gmail.com>
2021-07-03 13:39:21 -0700

    add solution to exercise 13

--- methodsAndFunctions/13-usingBooleans.py
+++ methodsAndFunctions/13-usingBooleans.py
@@ -1,3 +1,9 @@
 # Define a function called myfunc that takes three arguments, x, y and z.
 # If z is True return x. If z is false return y.
 # Remember, don't run the function, simply provide the definition.
+
+def myfunc(x,y,z):
+    if z == True:
+        return x
+    elif z == False:
+        return y
