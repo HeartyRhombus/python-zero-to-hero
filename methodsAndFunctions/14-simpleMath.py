63de4607669c049f83f4c2aeed2705b40d4705bd
HeartyRhombus <kaylahmg@gmail.com>
2021-07-03 13:39:53 -0700

    add solution to exercise 14

--- methodsAndFunctions/14-simpleMath.py
+++ methodsAndFunctions/14-simpleMath.py
@@ -1,2 +1,5 @@
 # Define a function called myfunc that takes in two arguments and returns their sum.
 # Remember, don't run the function, simply provide the definition.
+
+def myfunc(a,b):
+    return a + b
